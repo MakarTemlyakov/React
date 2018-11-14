da211ded5ad17d2b513fb338ba4ae71bd44be579
Makar <makartemlyakov@gmail.com>
2018-11-14 22:54:22 +0300

    Update Article.js

--- my-app1/src/components/Article.js
+++ my-app1/src/components/Article.js
@@ -1,9 +1,59 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-class Article extends React.Component{
-    state = {
-        visible: false,
+
+class ShowComments extends React.Component{
+    render() {
+        return (
+            <div>
+                <p><strong>Комментарии:</strong><br/></p>
+                {this.props.comments.map((comment,index)=>{
+                    return <p>>><em>{comment}</em></p>
+                })}
+            </div>
+        )
     }
+}
+class AddComment extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            newComment:" "
+        }
+        this.handChange = this.handChange.bind(this);
+        this.handAddComment = this.handAddComment.bind(this);
+    }
+    handChange(e){
+        this.setState({newComment:e.target.value})
+    }
+    handAddComment(){
+        this.props.addNewComment(this.state.newComment)
+        this.setState({newComment:''})
+    }
+    render() {
+        return (
+            <div>
+                <input type="text" value={this.state.newComment} onChange={this.handChange} />
+                <button onClick={this.handAddComment}>Add comment</button>
+            </div>
+        )
+    }
+}
+
+class Article extends React.Component{
+    constructor(props){
+        super(props);
+        
+        this.state = {
+            visible: false,
+            comments:['Стандартный коммент'],
+        }
+        this.addComment = this.addComment.bind(this);
+    }
+    addComment(comment) {
+        this.setState((state) => ({
+          comments: state.comments.concat([comment])
+        }))
+      }
     handReadMoreClck = (e) => {
         e.preventDefault();
         this.setState({ visible: true })
@@ -16,15 +66,23 @@ class Article extends React.Component{
                 <p className="news__author">{author}:</p>
                 <p className="news__text">{text}</p>
                 {
-                    !visible && <a href="#readmore" className='news__readmore' onClick={this.handReadMoreClck}>More...</a>
+                    !visible && <a href="#readmore" className='news__readmore' onClick={this.handReadMoreClck} >More...</a>
                 }
                 {
-                    visible && <p className="news__big-text">{bigText}</p>
+                    visible &&<div className="news__big-text">{bigText}<br/>
+                                <ShowComments comments={this.state.comments}/>
+                                <AddComment addNewComment={this.addComment}/>   
+                             </div>   
                 }
+                
             </div>
         )
     }
 }
+AddComment.propTypes = {
+    addNewComment: PropTypes.func.isRequired
+}
+Article.defaultProps = { comments:[]}
 Article.propTypes = {
     data:PropTypes.shape({
     id:PropTypes.number.isRequired,
