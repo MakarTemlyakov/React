324089040bb6f87797104071838d1893f9a6353e
Makar <makartemlyakov@gmail.com>
2018-11-14 14:52:55 +0300

    Update News.js

--- my-app1/src/components/News.js
+++ my-app1/src/components/News.js
@@ -3,32 +3,31 @@ import PropTypes from 'prop-types'
 import { Article } from './Article'
 
 class News extends React.Component {
-  renderNews = () => {
-    const { data } = this.props
-    let newsTemplate = null
-
-    if (data.length) {
-      newsTemplate = data.map(function(item) {
-        return <Article key={item.id} data={item} />
-      })
-    } else {
-      newsTemplate = <p>You don't have new news</p>
-    }
-    return newsTemplate
-  }
-  render() {
-    const { data } = this.props
-
-  return (
-         <div className='news'>
-           {this.renderNews()}
-           {
-             data.length ? <strong className={'news__count'}>Всего новостей: {data.length}</strong> : null
-           }
-          </div>
-       );
-     }
-   }
+        renderNews = () => {
+          const { data } = this.props
+          let newsTemplate = null
+          if (data.length) {
+            newsTemplate = data.map(function(item) {
+              return <Article key={item.id} data={item}/>
+            })
+          } else {
+            newsTemplate = <p>You dont have news</p>
+          }
+          return newsTemplate
+        }
+        render() {
+          const { data } = this.props
+  
+          return (
+            <div className='news'>
+              {this.renderNews()}
+              {
+                data.length ? <strong className={'news__count'}>All news: {data.length}</strong> : null
+              }
+            </div>
+          );
+        }
+      }
           
 News.propTypes = {
   data: PropTypes.array.isRequired,
